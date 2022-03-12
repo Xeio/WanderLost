9cc64846959ef517e7346bc2e16286b73e1b68dc
Joshua Shaffer <xeio@live.com>
2022-03-12 17:04:27 -0500

    Remember server between sessions

---
+++ WanderLost/WanderLost/Client/ClientData.cs
@@ -0,0 +1,8 @@
+﻿namespace WanderLost.Client
+{
+    public class ClientData
+    {
+        public string Region { get; set; } = string.Empty;
+        public string Server { get; set; } = string.Empty;
+    }
+}
--- WanderLost/WanderLost/Client/Program.cs
+++ WanderLost/WanderLost/Client/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WanderLost.Client;
@@ -7,5 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
