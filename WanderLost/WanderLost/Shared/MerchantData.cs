736e305df878461c7a310c5dfe19d21f04bb32c6
Joshua Shaffer <xeio@live.com>
2022-03-12 20:46:15 -0500

    Add merchant zone data

--- WanderLost/WanderLost/Shared/MerchantData.cs
+++ WanderLost/WanderLost/Shared/MerchantData.cs
@@ -5,5 +5,6 @@
         public string Name { get; init; } = string.Empty;
         public string Region { get; init; } = string.Empty;
         public List<TimeSpan> AppearanceTimes { get; init; } = new();
+        public List<string> Zones { get; init; } = new();
     }
 }
---
+++ WanderLost/WanderLost/Shared/Utils.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+
+namespace WanderLost.Shared
+{
+    public static class Utils
+    {
+        public static JsonSerializerOptions JsonOptions { get; } = new JsonSerializerOptions(JsonSerializerDefaults.Web)
+        {
+            AllowTrailingCommas = true,
+            ReadCommentHandling = JsonCommentHandling.Skip,
+        };
+    }
+}
