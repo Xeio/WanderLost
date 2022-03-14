ffcc4dc9fb790955d9c0e22a5c9e75d5c791e41e
Joshua Shaffer <xeio@live.com>
2022-03-14 02:13:43 -0400

    Add support to display items with rarity color

---
+++ WanderLost/WanderLost/Shared/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WanderLost.Shared
+{
+    public class Item
+    {
+        public string Name { get; init; } = string.Empty;
+        public Rarity Rarity { get; init; }
+    }
+}
