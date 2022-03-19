4df148728c55ee4419debe410dd5f5bc272370d9
Joshua Shaffer <xeio@live.com>
2022-03-19 16:13:44 -0400

    Centralize client data initialization

---
+++ WanderLost/WanderLost/Client/ClientStaticDataController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+using WanderLost.Shared;
+
+namespace WanderLost.Client
+{
+    public class ClientStaticDataController
+    {
+        public bool Initialized { get; private set; }
+        public Dictionary<string, ServerRegion> ServerRegions { get; private set; } = new();
+        public Dictionary<string, MerchantData> Merchants { get; private set; } = new();
+
+        private readonly NavigationManager _navigationManager;
+        private readonly HttpClient _httpClient;
+
+        public ClientStaticDataController(NavigationManager navigationManager, HttpClient httpClient)
+        {
+            _navigationManager = navigationManager;
+            _httpClient = httpClient;
+        }
+
+        public async Task Init()
+        {
+            if (Initialized) return;
+
+            Merchants = await _httpClient.GetFromJsonAsync<Dictionary<string, MerchantData>>(_navigationManager.ToAbsoluteUri("/data/merchants.json"), Utils.JsonOptions) ?? new();
+            ServerRegions = await _httpClient.GetFromJsonAsync<Dictionary<string, ServerRegion>>(_navigationManager.ToAbsoluteUri("/data/servers.json"), Utils.JsonOptions) ?? new();
+
+            Initialized = true;
+        }
+    }
+}
--- WanderLost/WanderLost/Client/Program.cs
+++ WanderLost/WanderLost/Client/Program.cs
@@ -10,6 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped<MerchantHubClient>();
+builder.Services.AddScoped<ClientStaticDataController>();
 
 builder.Services.AddBlazoredLocalStorage();
 
