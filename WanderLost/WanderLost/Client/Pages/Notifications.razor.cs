c6f737339c261aee2f1a2dfca78875df6481e058
Cyco0815 <101972110+Cyco0815@users.noreply.github.com>
2022-03-21 23:04:39 +0100

    Added Notification Page for future settings.

---
+++ WanderLost/WanderLost/Client/Pages/Notifications.razor.cs
@@ -0,0 +1,12 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+
+namespace WanderLost.Client.Pages
+{
+    public partial class Notifications
+    {
+        [Inject] public ILocalStorageService LocalStorage { get; set; } = default!; //default! to suppress NULL warning
+
+        //todo
+    }
+}
