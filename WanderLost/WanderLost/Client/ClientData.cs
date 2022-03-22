8aad583ee532bd4eddf4788362a913b7f31deae2
Cyco0815 <101972110+Cyco0815@users.noreply.github.com>
2022-03-22 22:50:14 +0100

    Fixed issues with Notifications page. BlazoredStorage write/read works now.

--- WanderLost/WanderLost/Client/ClientData.cs
+++ WanderLost/WanderLost/Client/ClientData.cs
@@ -6,6 +6,6 @@ namespace WanderLost.Client
     {
         public string Region { get; set; } = string.Empty;
         public string Server { get; set; } = string.Empty;
-        public List<MerchantData>? NotifyingMerchants; //null == unset
+        public List<MerchantData>? NotifyingMerchants { get; set; } //null == unset
     }
 }
