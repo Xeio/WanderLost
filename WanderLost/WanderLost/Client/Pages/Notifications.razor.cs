476ec2b91a8ff1fda458cea3747df063331bc8ab
Cyco0815 <101972110+Cyco0815@users.noreply.github.com>
2022-03-22 23:08:39 +0100

    Added TestNotifications to Notifications page.

--- WanderLost/WanderLost/Client/Pages/Notifications.razor.cs
+++ WanderLost/WanderLost/Client/Pages/Notifications.razor.cs
@@ -10,10 +10,12 @@ namespace WanderLost.Client.Pages
         [Inject] public ILocalStorageService LocalStorage { get; set; } = default!; //default! to suppress NULL warning
         [Inject] public ClientStaticDataController StaticData { get; set; } = default!;
         [Inject] public NavigationManager NavigationManager { get; set; } = default!;
+        [Inject] public ClientNotificationService ClientNotifications { get; set; } = default!; //default! to suppress NULL warning
 
         private ClientData? ClientData;
         protected override async Task OnInitializedAsync()
         {
+            _ = ClientNotifications.Init();
             await StaticData.Init();
             ClientData = await LocalStorage.GetItemAsync<ClientData?>(nameof(ClientData));
             if (ClientData == null)
@@ -25,6 +27,44 @@ namespace WanderLost.Client.Pages
                 ClientData = await LocalStorage.GetItemAsync<ClientData?>(nameof(ClientData));
             }
             await base.OnInitializedAsync();
+        }
+
+        private T pickRandomElement<T>(IEnumerable<T> list)
+        {
+            var r = new Random((int)DateTime.Now.Ticks);
+            return list.ElementAt(r.Next(0, list.Count()));
+        }
+
+        protected async Task OnTestMerchantSpawnClicked()
+        {
+            await ClientNotifications.Init();
+
+            var dummyData = pickRandomElement(StaticData.Merchants).Value;
+            var dummyMerchantGroup = new ActiveMerchantGroup
+            {
+                MerchantData = dummyData,
+                MerchantName = dummyData.Name,
+                ActiveMerchants = new List<ActiveMerchant> { new ActiveMerchant { Name = dummyData.Name } },
+
+            };
+            await ClientNotifications.CreateMerchantSpawnNotification(dummyMerchantGroup);
+        }
+
+
+        protected async Task OnTestMerchantFoundClicked()
+        {
+            await ClientNotifications.Init();
+
+            var dummyData = pickRandomElement(StaticData.Merchants).Value;
+            var dummyMerchantGroup = new ActiveMerchantGroup
+            {
+                MerchantData = dummyData,
+                MerchantName = dummyData.Name,
+                ActiveMerchants = new List<ActiveMerchant> { new ActiveMerchant { Name = dummyData.Name, Card = pickRandomElement(dummyData.Cards), Zone = pickRandomElement(dummyData.Zones) } },
+
+            };
+            await ClientNotifications.CreateMerchantFoundNotification(dummyMerchantGroup);
+
         }
 
         protected async Task OnNotificationStateChanged(bool setActive, string category, string merchant, object value)
