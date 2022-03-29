0664dd40506147cd67471d69a9116e9cb5206f22
Osman Tuna <yazilimger@gmail.com>
2022-03-29 11:41:01 +0300

    sound notification
    saves notification settings in local storage and notify user with sound when favorited card or rapport appeared

---
+++ WanderLost/WanderLost/Client/NotificationSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace WanderLost.Client
+{
+    public class NotificationSettings
+    {
+        public NotificationSettings()
+        {
+            CardList = new List<string>();
+        }
+        public List<string> CardList { get; set; }
+        public bool NotifyLegendaryRapport { get; set; }
+    }
+}
---
+++ WanderLost/WanderLost/Client/wwwroot/js/sounds.js
@@ -0,0 +1,11 @@
+﻿window.PlayAudioFile = (src) => {
+    var audio = document.getElementById('player');
+    if (audio != null) {
+        var audioSource = document.getElementById('playerSource');
+        if (audioSource != null) {
+            audioSource.src = src;
+            audio.load();
+            audio.play();
+        }
+    }
+}
