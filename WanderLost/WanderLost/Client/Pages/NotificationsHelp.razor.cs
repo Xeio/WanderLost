0b5b38c567ef541312c81d68f494bcdfa35e9fa3
Cyco0815 <101972110+Cyco0815@users.noreply.github.com>
2022-04-14 19:27:42 +0200

    Added a custom popup/toast in Merchants page when user has enabled browser-notification-sounds and hasn't clicked anywhere yet. Extended NotificationsHelp with explanation of said popup.

---
+++ WanderLost/WanderLost/Client/Pages/NotificationsHelp.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace WanderLost.Client.Pages
+{
+    public partial class NotificationsHelp
+    {
+        [Inject] public NavigationManager NavigationManager { get; set; } = default!;
+
+        [Parameter] public string? Section { get; set; }
+
+        private Dictionary<string, bool> _initialSectionState = new Dictionary<string, bool>();
+        protected override void OnInitialized()
+        {
+            initSections();
+            if (!string.IsNullOrEmpty(Section))
+            {
+                _initialSectionState[Section] = true;
+            }
+            base.OnInitialized();
+        }
+
+        private void initSections()
+        {
+            _initialSectionState.Add("enableNotif", false);
+            _initialSectionState.Add("unresponsiveNotif", false);
+            _initialSectionState.Add("noNotif", false);
+            _initialSectionState.Add("missingNotif", false);
+            _initialSectionState.Add("setupNotif", false);
+            _initialSectionState.Add("notifsoundpopup", false);
+        }
+    }
+}
