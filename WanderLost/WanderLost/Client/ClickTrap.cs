af70b9bb61d118047a3459915ca5d2fa919b33e1
Cyco0815 <101972110+Cyco0815@users.noreply.github.com>
2022-04-14 19:27:42 +0200

    Added a custom popup/toast in Merchants page when user has enabled browser-notification-sounds and hasn't clicked anywhere yet. Extended NotificationsHelp with explanation of said popup.

---
+++ WanderLost/WanderLost/Client/ClickTrap.cs
@@ -0,0 +1,23 @@
+﻿namespace WanderLost.Client
+{
+    public class ClickTrap
+    {
+        public event EventHandler<bool>? HasChanged;
+
+        private bool _hasClicked;
+
+        public bool HasClicked
+        {
+            get { return _hasClicked; }
+            set 
+            {
+                if (HasClicked != value)
+                {
+                    _hasClicked = value;
+                    HasChanged?.Invoke(this, value);
+                }
+            }
+        }
+
+    }
+}
