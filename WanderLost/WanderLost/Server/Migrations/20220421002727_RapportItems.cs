2cbec9cbd9b3d723bb5cd9c8e56ef66e598e8bc8
Sedro <sedro01@gmail.com>
2022-04-20 17:28:44 -0700

    generate EF migration

---
+++ WanderLost/WanderLost/Server/Migrations/20220421002727_RapportItems.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WanderLost.Server.Migrations
+{
+    public partial class RapportItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RapportRarity",
+                table: "ActiveMerchants");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rapport_Name",
+                table: "ActiveMerchants",
+                type: "nvarchar(40)",
+                maxLength: 40,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Rapport_Rarity",
+                table: "ActiveMerchants",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rapport_Name",
+                table: "ActiveMerchants");
+
+            migrationBuilder.DropColumn(
+                name: "Rapport_Rarity",
+                table: "ActiveMerchants");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RapportRarity",
+                table: "ActiveMerchants",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
