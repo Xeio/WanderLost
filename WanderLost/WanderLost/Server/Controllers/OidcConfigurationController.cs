346167c041774e311eb1ec9e95ca6f08c0d06add
Joshua Shaffer <xeio@live.com>
2022-04-27 01:37:02 -0400

    Fix OIDC controller namespace

--- WanderLost/WanderLost/Server/Controllers/OidcConfigurationController.cs
+++ WanderLost/WanderLost/Server/Controllers/OidcConfigurationController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BlazorAuthServerTest.Server.Controllers
+namespace WanderLost.Server.Controllers
 {
     public class OidcConfigurationController : Controller
     {
