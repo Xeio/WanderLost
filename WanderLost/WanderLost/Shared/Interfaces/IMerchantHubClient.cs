b737cd9e7a41191b5108011f4316b61155fe73dd
Joshua Shaffer <xeio@live.com>
2022-05-01 13:55:29 -0400

    Remove UpdateMerchant client method that was never called from server

--- WanderLost/WanderLost/Shared/Interfaces/IMerchantHubClient.cs
+++ WanderLost/WanderLost/Shared/Interfaces/IMerchantHubClient.cs
@@ -4,7 +4,6 @@ namespace WanderLost.Shared.Interfaces
 {
     public interface IMerchantHubClient
     {
-        Task UpdateMerchant(string server, ActiveMerchant merchant);
         Task UpdateMerchantGroup(string server, ActiveMerchantGroup merchantGroup);
         Task UpdateVoteTotal(Guid merchantId, int voteTotal);
         Task UpdateVoteSelf(Guid merchantId, VoteType voteType);
