230cc476d7d6144d290a84a30cc429b920a0f454
Joshua Shaffer <xeio@live.com>
2022-05-03 00:49:22 -0400

    Clear Blazor cache when reloading from error UI

---
+++ WanderLost/WanderLost/Client/wwwroot/js/ErrorHandling.js
@@ -0,0 +1,12 @@
+﻿async function clearLocalstorageCacheAndReload() {
+    try {
+        //If the blazor UI has an error, just forcibly clear the local storage cache when reloading
+        //Some users saw instances where clearing the cached DLLs helped, possibly rare CDN issue when purging caches for new update?
+        //Ideally this should be rare enough not to care about the extra requests it causes
+        await caches.delete((await caches.keys())[0]);
+    }
+    catch {
+        //Don't really care about errors clearing cache
+    }
+    location.reload();
+}
