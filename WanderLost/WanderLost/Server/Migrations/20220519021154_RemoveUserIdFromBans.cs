f45f9c6b5c7317715367149ae853fe528f9694a3
Joshua Shaffer <xeio@live.com>
2022-05-18 22:12:32 -0400

    Remove UserId from Bans table

---
+++ WanderLost/WanderLost/Server/Migrations/20220519021154_RemoveUserIdFromBans.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WanderLost.Server.Migrations
+{
+    public partial class RemoveUserIdFromBans : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Bans");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Bans",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: true);
+        }
+    }
+}
