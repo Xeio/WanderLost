cc7c4fb77637b4c0e57c1c74cbae2a24af273840
Joshua Shaffer <xeio@live.com>
2022-05-19 23:16:53 -0400

    Add PushNotification management web API

---
+++ WanderLost/WanderLost/Server/Controllers/PushNotificationApi.cs
@@ -0,0 +1,77 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WanderLost.Shared.Data;
+
+namespace WanderLost.Server.Controllers
+{
+    [ApiController]
+    [Route("api/PushNotifications")]
+    public class PushNotificationsController : ControllerBase
+    {
+        private readonly MerchantsDbContext _merchantsDbContext;
+
+        public PushNotificationsController(MerchantsDbContext merchantsDbContext)
+        {
+            _merchantsDbContext = merchantsDbContext;
+        }
+
+        [HttpGet]
+        [Route(nameof(GetPushSubscription))]
+        public async Task<PushSubscription?> GetPushSubscription([FromBody] string clientToken)
+        {
+            if (string.IsNullOrEmpty(clientToken)) return null;
+
+            return await _merchantsDbContext.PushSubscriptions
+                .TagWithCallSite()
+                .AsNoTracking()
+                .FirstOrDefaultAsync(s => s.Token == clientToken);
+        }
+
+        [HttpPost]
+        [Route(nameof(UpdatePushSubscription))]
+        public async Task<StatusCodeResult> UpdatePushSubscription([FromBody] PushSubscription subscription)
+        {
+            if (string.IsNullOrEmpty(subscription.Token)) return BadRequest();
+
+            bool exists = await _merchantsDbContext.PushSubscriptions
+                            .TagWithCallSite()
+                            .AnyAsync(s => s.Token == subscription.Token);
+            if (exists)
+            {
+                _merchantsDbContext.Entry(subscription).State = EntityState.Modified;
+            }
+            else
+            {
+                _merchantsDbContext.Add(subscription);
+            }
+            _merchantsDbContext.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route(nameof(RemovePushSubscription))]
+        public async Task<StatusCodeResult> RemovePushSubscription([FromBody] string clientToken)
+        {
+            if (string.IsNullOrEmpty(clientToken)) return BadRequest();
+
+            try
+            {
+                var subscription = new PushSubscription()
+                {
+                    Token = clientToken,
+                };
+                _merchantsDbContext.Entry(subscription).State = EntityState.Deleted;
+                await _merchantsDbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                //If a subscription didn't exist, just ignore the error.
+                //Probably happens mainly if a user multi-clicks delete before the request has completed
+            }
+
+            return Ok();
+        }
+    }
+}
