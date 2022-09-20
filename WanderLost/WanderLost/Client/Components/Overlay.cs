f3f9a1c3374c97c5d21433f5ff25c730a59fb43d
Dmitrii Samsonov <dsamsonov@microsoft.com>
2022-09-20 14:51:57 +0200

    add overlay

---
+++ WanderLost/WanderLost/Client/Components/Overlay.cs
@@ -0,0 +1,10 @@
+﻿using WanderLost.Shared.Data;
+
+namespace WanderLost.Client.Components
+{
+    public interface IOverlay
+    {
+        public void ShowMerchantGroup(ActiveMerchantGroup MerchantGroup, Dictionary<Guid, VoteType> Votes, string Server);
+    }
+}
+
