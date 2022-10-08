22cd61cb119dd0105554566185688fe2e3057814
Joshua Shaffer <xeio@live.com>
2022-10-08 01:15:28 -0400

    Nullability, whitespace, and style fixes

---
+++ WanderLost/WanderLost/Client/Components/IOverlay.cs
@@ -0,0 +1,10 @@
+﻿using WanderLost.Shared.Data;
+
+namespace WanderLost.Client.Components
+{
+    public interface IOverlay
+    {
+        public Task ShowMerchantGroup(ActiveMerchantGroup merchantGroup, string server, string region);
+        public Task ShowMap(string zone);
+    }
+}
--- WanderLost/WanderLost/Client/Components/Overlay.razor.cs
+++ WanderLost/WanderLost/Client/Components/Overlay.razor.cs
@@ -7,69 +7,68 @@ namespace WanderLost.Client.Components;
 
 public partial class Overlay : IOverlay, IDisposable
 {
-  [Inject]
-  private NavigationManager NavigationManager { get; set; }
+    [Inject]
+    private NavigationManager NavigationManager { get; set; } = default!;
 
-  [Inject]
-  private IJSRuntime JSRuntime { get; set; }
+    [Inject]
+    private IJSRuntime JSRuntime { get; set; } = default!;
 
-  [Parameter]
-  public RenderFragment? ChildContent { get; set; }
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
 
-  public ActiveMerchantGroup? MerchantGroup;
-  public string? Server;
-  public string? Zone;
-  public string? Region;
+    public ActiveMerchantGroup? MerchantGroup;
+    public string? Server;
+    public string? Zone;
+    public string? Region;
 
-
-  public bool isActive
-  {
-    get
+    public bool IsActive
     {
-      return ((this.MerchantGroup is not null) && (this.Server is not null)) || (this.Zone is not null);
+        get
+        {
+            return (MerchantGroup is not null && Server is not null) || Zone is not null;
+        }
     }
-  }
 
-  public void ShowMap(string Zone)
-  {
-    this.MerchantGroup = null;
-    this.Server = null;
-    this.Region = null;
-    this.Zone = Zone;
-    JSRuntime.InvokeVoidAsync("HideBodyScroll");
-  }
+    public async Task ShowMap(string zone)
+    {
+        MerchantGroup = null;
+        Server = null;
+        Region = null;
+        Zone = zone;
+        await JSRuntime.InvokeVoidAsync("HideBodyScroll");
+    }
 
-  public void ShowMerchantGroup(ActiveMerchantGroup MerchantGroup, string Server, string Region)
-  {
-    this.MerchantGroup = MerchantGroup;
-    this.Server = Server;
-    this.Region= Region;
-    this.Zone = null;
-    JSRuntime.InvokeVoidAsync("HideBodyScroll");
-  }
+    public async Task ShowMerchantGroup(ActiveMerchantGroup merchantGroup, string server, string region)
+    {
+        MerchantGroup = merchantGroup;
+        Server = server;
+        Region = region;
+        Zone = null;
+        await JSRuntime.InvokeVoidAsync("HideBodyScroll");
+    }
 
-  public void Close()
-  {
-    this.MerchantGroup = null;
-    this.Server = null;
-    this.Region = null;
-    this.Zone = null;
-    JSRuntime.InvokeVoidAsync("ShowBodyScroll");
-  }
+    public async Task Close()
+    {
+        MerchantGroup = null;
+        Server = null;
+        Region = null;
+        Zone = null;
+        await JSRuntime.InvokeVoidAsync("ShowBodyScroll");
+    }
 
-  protected override void OnInitialized()
-  {
-    NavigationManager.LocationChanged += HandleLocationChanged;
-  }
+    protected override void OnInitialized()
+    {
+        NavigationManager.LocationChanged += HandleLocationChanged;
+    }
 
-  private void HandleLocationChanged(object? sender, LocationChangedEventArgs e)
-  {
-    Close();
-  }
+    private async void HandleLocationChanged(object? sender, LocationChangedEventArgs e)
+    {
+        await Close();
+    }
 
-  public void Dispose()
-  {
-    NavigationManager.LocationChanged -= HandleLocationChanged;
-  }
+    public void Dispose()
+    {
+        NavigationManager.LocationChanged -= HandleLocationChanged;
+    }
 }
 
