5f7c42dbb3ef47070ed5ab98086b06b56c9e22b6
Joshua Shaffer <xeio@live.com>
2023-02-18 23:59:09 -0500

    Add Firebase to android

--- WanderLost/LostMerchants/Platforms/Android/MainActivity.cs
+++ WanderLost/LostMerchants/Platforms/Android/MainActivity.cs
@@ -1,11 +1,20 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Firebase;
 
 namespace LostMerchants
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            var app = FirebaseApp.InitializeApp(ApplicationContext);
+
+            System.Diagnostics.Debug.Assert(app is not null, "Failed to initialize firebase.");
+        }
     }
 }
