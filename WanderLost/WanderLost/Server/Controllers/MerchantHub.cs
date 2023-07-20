36e65fe0d64b623569fa943ae1a14e5fa0b0e776
nobbyfix <nobbyfix@users.noreply.github.com>
2023-07-20 19:01:30 +0200

    allow to select legendary card to display card stats of

--- WanderLost/WanderLost/Server/Controllers/MerchantHub.cs
+++ WanderLost/WanderLost/Server/Controllers/MerchantHub.cs
@@ -340,19 +340,19 @@ public class MerchantHub : Hub<IMerchantHubClient>, IMerchantHubServer
         };
     }
 
-    public async Task<WeiStats> GetWeiStats()
+    public async Task<CardStats> GetCardStats(string cardName)
     {
-        return await _memoryCache.GetOrCreateAsync(nameof(GetWeiStats), async (cacheEntry) =>
+        return await _memoryCache.GetOrCreateAsync(cardName, async (cacheEntry) =>
         {
             cacheEntry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5);
 
             await _merchantsDbContext.Database.BeginTransactionAsync(System.Data.IsolationLevel.ReadUncommitted);
 
-            WeiStats stats = new();
+            CardStats stats = new();
 
-            var weiCounts = await _merchantsDbContext.ActiveMerchants
+            var cardCounts = await _merchantsDbContext.ActiveMerchants
                 .TagWithCallSite()
-                .Where(m => m.Card.Name == "Wei" && !m.Hidden && m.Votes > 0)
+                .Where(m => m.Card.Name == cardName && !m.Hidden && m.Votes >= 0)
                 .GroupBy(m => m.ActiveMerchantGroup.Server, (server, rows) => new
                 {
                     Server = server,
@@ -361,9 +361,9 @@ public class MerchantHub : Hub<IMerchantHubClient>, IMerchantHubServer
                 .OrderByDescending(i => i.Count)
                 .ToListAsync();
 
-            var recentWeis = await _merchantsDbContext.ActiveMerchants
+            var recentAppearances = await _merchantsDbContext.ActiveMerchants
                 .TagWithCallSite()
-                .Where(m => m.Card.Name == "Wei" && !m.Hidden && m.Votes > 0)
+                .Where(m => m.Card.Name == cardName && !m.Hidden && m.Votes >= 0)
                 .OrderByDescending(m => m.ActiveMerchantGroup.NextAppearance)
                 .Select(m => new { m.ActiveMerchantGroup.Server, m.ActiveMerchantGroup.NextAppearance })
                 .Take(50)
@@ -371,10 +371,10 @@ public class MerchantHub : Hub<IMerchantHubClient>, IMerchantHubServer
 
             await _merchantsDbContext.Database.RollbackTransactionAsync();
 
-            return new WeiStats()
+            return new CardStats()
             {
-                ServerWeiCounts = weiCounts.Select(c => (c.Server, c.Count)).ToList(),
-                RecentWeis = recentWeis.Select(r => (r.Server, r.NextAppearance)).ToList()
+                ServerCardCounts = cardCounts.Select(c => (c.Server, c.Count)).ToList(),
+                RecentAppearances = recentAppearances.Select(r => (r.Server, r.NextAppearance)).ToList()
             };
         }) ?? new();
     }
