60e634a3107c630f8f40116b47871e8649cd0ce4
Joshua Shaffer <xeio@live.com>
2023-12-16 15:10:41 -0500

    Add metric for discord interactions

--- WanderLost/WanderLost/Server/Discord/DiscordBotService.cs
+++ WanderLost/WanderLost/Server/Discord/DiscordBotService.cs
@@ -1,5 +1,6 @@
 ﻿using Discord;
 using Discord.WebSocket;
+using System.Diagnostics.Metrics;
 
 namespace WanderLost.Server.Discord;
 
@@ -8,12 +9,16 @@ public class DiscordBotService : BackgroundService
     private readonly IServiceProvider _services;
     private readonly ILogger<DiscordBotService> _logger;
     private readonly DiscordSocketClient _discordClient;
+    private readonly Counter<int> _interactionsCounter;
 
-    public DiscordBotService(ILogger<DiscordBotService> logger, IServiceProvider services, DiscordSocketClient discordClient)
+    public DiscordBotService(ILogger<DiscordBotService> logger, IServiceProvider services, DiscordSocketClient discordClient, IMeterFactory meterFactory)
     {
         _logger = logger;
         _services = services;
         _discordClient = discordClient;
+
+        var meter = meterFactory.Create("LostMerchants");
+        _interactionsCounter = meter.CreateCounter<int>("discord.interactions");
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -49,12 +54,14 @@ public class DiscordBotService : BackgroundService
             && arg is SocketUserMessage userMessage
             && arg.Channel is IDMChannel)
         {
+            _interactionsCounter.Add(1, new KeyValuePair<string, object?>("interactionType", nameof(MessageReceived)));
             await userMessage.ReplyAsync($"To set up a subscription use the {Format.Code("/manage-merchant-notifications")} command.");
         }
     }
 
     private async Task ModalSubmitted(SocketModal arg)
     {
+        _interactionsCounter.Add(1, new KeyValuePair<string, object?>("interactionType", nameof(ModalSubmitted)));
         using var scope = _services.CreateScope();
         foreach (var command in scope.ServiceProvider.GetServices<IDiscordCommand>())
         {
@@ -64,6 +71,7 @@ public class DiscordBotService : BackgroundService
 
     private async Task ButtonExecuted(SocketMessageComponent arg)
     {
+        _interactionsCounter.Add(1, new KeyValuePair<string, object?>("interactionType", nameof(ButtonExecuted)));
         using var scope = _services.CreateScope();
         foreach (var command in scope.ServiceProvider.GetServices<IDiscordCommand>())
         {
@@ -73,6 +81,7 @@ public class DiscordBotService : BackgroundService
 
     private async Task SlashCommandExecuted(SocketSlashCommand arg)
     {
+        _interactionsCounter.Add(1, new KeyValuePair<string, object?>("interactionType", nameof(SlashCommandExecuted)));
         using var scope = _services.CreateScope();
         foreach (var command in scope.ServiceProvider.GetServices<IDiscordCommand>())
         {
@@ -82,6 +91,7 @@ public class DiscordBotService : BackgroundService
 
     private async Task SelectMenuExecuted(SocketMessageComponent arg)
     {
+        _interactionsCounter.Add(1, new KeyValuePair<string, object?>("interactionType", nameof(SelectMenuExecuted)));
         using var scope = _services.CreateScope();
         foreach (var command in scope.ServiceProvider.GetServices<IDiscordCommand>())
         {
