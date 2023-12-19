211c5f015332833f1a7212a5f19adf5d124a9492
Joshua Shaffer <xeio@live.com>
2023-12-19 18:13:07 -0500

    Misc Items DB context changes

---
+++ WanderLost/WanderLost/Server/Migrations/20231219231157_MiscItems.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WanderLost.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class MiscItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tradeskill",
+                table: "ActiveMerchants");
+
+            migrationBuilder.CreateTable(
+                name: "ActiveMerchants_MiscItems",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
+                    ActiveMerchantId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Rarity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActiveMerchants_MiscItems", x => new { x.ActiveMerchantId, x.Name });
+                    table.ForeignKey(
+                        name: "FK_ActiveMerchants_MiscItems_ActiveMerchants_ActiveMerchantId",
+                        column: x => x.ActiveMerchantId,
+                        principalTable: "ActiveMerchants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ActiveMerchants_MiscItems");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tradeskill",
+                table: "ActiveMerchants",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+    }
+}
