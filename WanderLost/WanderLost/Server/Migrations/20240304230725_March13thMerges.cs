54d5a54320fcb98d9bc05c983728dfaad6f64667
Joshua Shaffer <xeio@live.com>
2024-03-04 18:09:12 -0500

    March 13th Server Merges

---
+++ WanderLost/WanderLost/Server/Migrations/20240304230725_March13thMerges.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using WanderLost.Shared;
+
+#nullable disable
+
+namespace WanderLost.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class March13thMerges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            foreach (var pair in Utils.ServerMerges)
+            {
+                migrationBuilder.Sql($"UPDATE PushSubscriptions SET Server = '{pair.Value}' WHERE Server = '{pair.Key}';");
+                migrationBuilder.Sql($"UPDATE DiscordNotifications SET Server = '{pair.Value}' WHERE Server = '{pair.Key}';");
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
